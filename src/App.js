608405d181a565898eea2667ed2c5a1e6513c04d
dlbeadle <donaldleebeadle@gmail.com>
2020-05-14 08:51:48 -0500

    All tests passing for first two user stories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
+// App.js
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+class App extends React.Component {
+  state = {
+    isAddRecipeFormDisplayed: false,
+    recipes: [],
+    newRecipeName: "",
+    newRecipeInstructions: ""
+  }
+
+  toggleAddRecipeForm = () => {
+    this.setState({isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed})
+  }
+
+  submitRecipe = (event) => {
+    event.preventDefault()
+    this.setState({recipes: [
+        {
+          name: this.state.newRecipeName, 
+          instructions :this.state.newRecipeInstructions
+        }
+      ]
+    })
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+  
+    this.setState({[name]: target.value});
+  }
+
+  render(){
+    const addNewRecipeForm = (
+      <form id="recipe-form" onSubmit={this.submitRecipe}>
+        <label htmlFor="newRecipeName">Recipe name: </label>
+        <input type="text" name="newRecipeName" onChange={this.handleChange} value={this.state.newRecipeName} />
+        <label htmlFor="newRecipeInstructions">Instructions:</label>
+        <textarea name="newRecipeInstructions" 
+                  placeholder="write recipe instructions here..." 
+                  onChange={this.handleChange} 
+                  value={this.state.newRecipeInstructions} />
+        <input type="submit" />
+      </form>
+    )
+  
   return (
     <div className="App">
       <h1 className="App-header">My Recipes</h1>
-      <p>There are no recipes to list.</p>
+      {
+        this.state.isAddRecipeFormDisplayed 
+        ? addNewRecipeForm 
+        : <button id="add-recipe" onClick={this.toggleAddRecipeForm}>Add Recipe</button>
+      }
+      {
+        this.state.recipes.length > 0 ?
+        <ul>
+          <li>{ this.state.recipes[0].name }</li>
+        </ul> :
+        <p>There are no recipes to list.</p>
+      }
     </div>
-  );
+  )
+  }
 }
 
 export default App;
